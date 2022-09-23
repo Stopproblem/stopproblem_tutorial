096914c88fe68254eddf2c620bef06075a68ea96
Stopproblem <stopproblem@mail.ru>
2022-09-23 17:29:00 +0400

    +

---
+++ Autotests/test_items.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+import pytest
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_guest_should_see_add_in_basket(browser):
+    browser.get(link)
+    time.sleep(10)
+    assert browser.find_elements(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary")
---
+++ Test_project/conftest.py
@@ -0,0 +1,27 @@
+from selenium.webdriver.common.by import By
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+list_langs = ["es", "ru", "fr", "en"]
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en', help='Choose from langs: (en/ru/es/...)')
+    parser.addoption('--browser_name', action='store', default='chrome', help='Choose browser')
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    browser = None
+    if language in list_langs:
+        print(f"\nstart chrome browser for {language} language test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+    else:
+        raise pytest.UsageError("--language should be from the list " + str(list_langs))
+
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
