609ff3dec54b94a4d5f70b9d4955107466c154a8
Stopproblem <stopproblem@mail.ru>
2022-09-23 16:56:39 +0400

    Correct fixture

--- Autotests/conftest.py
+++ Autotests/conftest.py
@@ -1,26 +1,27 @@
 from selenium.webdriver.common.by import By
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+list_langs = ["es", "ru", "fr", "en"]
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='ru', help='Choose from langs: (en/ru/es/...)')
     parser.addoption('--browser_name', action='store', default='chrome', help='Choose browser')
 
-@pytest.fixture()
-def language(pytestconfig):
-    return pytestconfig.getoption('language')
 
-@pytest.fixture()
+@pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption('browser_name')
+    language = request.config.getoption("language")
     browser = None
-    if browser_name == 'chrome':
-        browser = webdriver.Chrome()
-    elif browser_name == 'firefox':
-        browser = webdriver.Firefox()
+    if language in list_langs:
+        print(f"\nstart chrome browser for {language} language test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
     else:
-        raise pytest.UsageError('browser should be chrome or firefox')
+        raise pytest.UsageError("--language should be from the list " + str(list_langs))
+
     yield browser
-    print('\n quit browser')
+    print("\nquit browser..")
     browser.quit()
