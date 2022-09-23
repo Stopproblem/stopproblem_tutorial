f1b80e734153c95a23bf51c65770280a44b45ccc
Stopproblem <stopproblem@mail.ru>
2022-09-23 16:26:15 +0400

    correct name test_function

--- Autotests/test_items.py
+++ Autotests/test_items.py
@@ -5,6 +5,6 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_guest_should_see_add_in_basket(browser):
     browser.get(link)
     assert browser.find_elements(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary")
