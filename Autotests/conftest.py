686a4f1004c50857671a9df3c77023e3a1004e71
Stopproblem <stopproblem@mail.ru>
2022-09-23 16:43:04 +0400

    optimization

--- Autotests/conftest.py
+++ Autotests/conftest.py
@@ -1,18 +1,26 @@
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import pytest
 from selenium import webdriver
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None, help='Choose from langs: (en/ru/es/...)')
+    parser.addoption('--language', action='store', default='ru', help='Choose from langs: (en/ru/es/...)')
+    parser.addoption('--browser_name', action='store', default='chrome', help='Choose browser')
 
+@pytest.fixture()
+def language(pytestconfig):
+    return pytestconfig.getoption('language')
 
 @pytest.fixture()
 def browser(request):
-    user_language = request.config.getoption('language')
-    options = Options()
-    options.add_experimental_option('prefs', {'until.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
+    browser_name = request.config.getoption('browser_name')
+    browser = None
+    if browser_name == 'chrome':
+        browser = webdriver.Chrome()
+    elif browser_name == 'firefox':
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError('browser should be chrome or firefox')
     yield browser
+    print('\n quit browser')
     browser.quit()
--- Autotests/test_items.py
+++ Autotests/test_items.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 import pytest
 from selenium.webdriver.common.by import By
-
+import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_add_in_basket(browser):
-    browser.get(link)
+def test_guest_should_see_add_in_basket(browser, language):
+    browser.get(link+language+'/catalogue/coders-at-work_207/')
+    time.sleep(10)
     assert browser.find_elements(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary")
