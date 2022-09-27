2aa3bd7a80e58015ac22523ebf5e0939831c427e
Stopproblem <stopproblem@mail.ru>
2022-09-27 19:24:00 +0400

    -

--- Test_project/test_main_page.py
+++ Test_project/test_main_page.py
@@ -2,10 +2,6 @@ from .pages.main_page import MainPage
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
