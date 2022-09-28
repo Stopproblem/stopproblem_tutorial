f1ed69c8dad2ef14deb6429bf9775626c8ac9faa
Stopproblem <stopproblem@mail.ru>
2022-09-28 15:25:20 +0400

    add name matches test

--- Test_project/test_product_page.py
+++ Test_project/test_product_page.py
@@ -10,4 +10,5 @@ def test_guest_can_add_product_to_basket(browser):
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_product_price_is_basket_price()
-
+    page.should_be_message_about_adding()
+    
